5632b892283b76d85f2e72ce218432edfc9e85b4
1010varun <varunagarwal10.2@gmail.com>
2022-11-20 14:36:18 +0530

    solved port error

--- app.js
+++ app.js
@@ -76,6 +76,7 @@ app.patch("/modifyTask", async (req, res)=>{
     }
 })
 
-app.listen(5000, () => {
-    console.log("server listening to port 5000..");
+const port = process.env.PORT || 5000 ;
+app.listen(port, () => {
+    console.log(`server listening to port ${port}`);
 })
